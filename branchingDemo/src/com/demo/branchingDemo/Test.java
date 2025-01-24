b9d108410af512892942802aa71f742565d95f2e
home <home@DESKTOP-VEKD7C1>
2025-01-24 11:52:34 +0530

    add  hw

--- branchingDemo/src/com/demo/branchingDemo/Test.java
+++ branchingDemo/src/com/demo/branchingDemo/Test.java
@@ -1,9 +1,17 @@
 package com.demo.branchingDemo;
 
 public class Test {
+	
+	public void m1()
+	{
+		
+	}
 
 	
 	public static void main(String[] args) {
+		
+		System.out.println("hello world")
+		;
 		
 	}
 	
